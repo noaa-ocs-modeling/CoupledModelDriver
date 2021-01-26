e8f0b3197ee083a34d9c88cb5d2158b202d4619d
zachary.burnett <zachary.burnett@noaa.gov>
2021-01-26 10:21:57 -0500

    add examples

---
+++ examples/hera/single_run_hsofs_sandy_hera.py
@@ -0,0 +1,66 @@
+#! /usr/bin/env python
+
+from datetime import datetime, timedelta
+from pathlib import Path
+import sys
+
+from adcircpy import Tides
+from adcircpy.forcing.waves.ww3 import WaveWatch3DataForcing
+from adcircpy.forcing.winds.atmesh import AtmosphericMeshForcing
+from nemspy import ModelingSystem
+from nemspy.model import ADCIRCEntry, AtmosphericMeshEntry, WaveMeshEntry
+
+sys.path.append(Path(__file__).parent.parent.parent.absolute())
+
+from coupledmodeldriver.adcirc import write_adcirc_configurations
+from coupledmodeldriver.job_script import HPC
+from coupledmodeldriver.utilities import repository_root
+
+DATA_DIRECTORY = repository_root() / 'examples/data'
+INPUT_DIRECTORY = Path("/scratch2/COASTAL/coastal/save/Saeed.Moghimi/setups/nems_inp/hsofs_grid_v1/")
+OUTPUT_DIRECTORY = DATA_DIRECTORY / 'configuration' / 'hsofs'
+
+if __name__ == '__main__':
+    runs = {f'nems_hsofs_test': (None, None)}
+
+    if not (INPUT_DIRECTORY / 'fort.14').exists():
+        raise RuntimeError(f'file not found at {INPUT_DIRECTORY / "fort.14"}')
+
+    # init tidal forcing and setup requests
+    tidal_forcing = Tides()
+    tidal_forcing.use_all()
+    wind_forcing = AtmosphericMeshForcing(17, 3600)
+    wave_forcing = WaveWatch3DataForcing(5, 3600)
+
+    nems = ModelingSystem(
+        start_time=datetime(2012, 10, 22, 6),
+        duration=timedelta(days=14.5),
+        interval=timedelta(hours=1),
+        atm=AtmosphericMeshEntry('/scratch2/COASTAL/coastal/save/Saeed.Moghimi/setups/nems_inp/'
+                                 'hsofs_forcings/san_v2/inp_atmesh/SANDY_HWRF_HSOFS_Nov2018.nc'),
+        wav=WaveMeshEntry('/scratch2/COASTAL/coastal/save/Saeed.Moghimi/setups/nems_inp/'
+                          'hsofs_forcings/san_v2/inp_ww3/ww3.HWRF.NOV2018.2012_sxy.nc'),
+        ocn=ADCIRCEntry(382),
+    )
+
+    nems.connect('ATM', 'OCN')
+    nems.connect('WAV', 'OCN')
+    nems.sequence = [
+        'ATM -> OCN',
+        'WAV -> OCN',
+        'ATM',
+        'WAV',
+        'OCN',
+    ]
+
+    write_adcirc_configurations(
+        nems,
+        runs,
+        INPUT_DIRECTORY,
+        OUTPUT_DIRECTORY,
+        name='nems_hsofs_test',
+        email_address='zachary.burnett@noaa.gov',
+        platform=HPC.HERA,
+        spinup=timedelta(days=12.5),
+        forcings=[tidal_forcing, wind_forcing, wave_forcing],
+    )
---
+++ examples/stampede2/single_run_hsofs_sandy_stampede2.py
@@ -0,0 +1,64 @@
+#! /usr/bin/env python
+
+from datetime import datetime, timedelta
+from pathlib import Path
+import sys
+
+from adcircpy import Tides
+from adcircpy.forcing.waves.ww3 import WaveWatch3DataForcing
+from adcircpy.forcing.winds.atmesh import AtmosphericMeshForcing
+from nemspy import ModelingSystem
+from nemspy.model import ADCIRCEntry, AtmosphericMeshEntry, WaveMeshEntry
+
+sys.path.append(Path(__file__).parent.parent.parent.absolute())
+
+from coupledmodeldriver.adcirc import write_adcirc_configurations
+from coupledmodeldriver.job_script import HPC
+from coupledmodeldriver.utilities import repository_root
+
+DATA_DIRECTORY = repository_root() / 'examples/data'
+INPUT_DIRECTORY = DATA_DIRECTORY / 'input' / 'hsofs'
+OUTPUT_DIRECTORY = DATA_DIRECTORY / 'configuration' / 'hsofs'
+
+if __name__ == '__main__':
+    runs = {f'nems_hsofs_test': (None, None)}
+
+    if not (INPUT_DIRECTORY / 'fort.14').exists():
+        raise RuntimeError(f'file not found at {INPUT_DIRECTORY / "fort.14"}')
+
+    # init tidal forcing and setup requests
+    tidal_forcing = Tides()
+    tidal_forcing.use_all()
+    wind_forcing = AtmosphericMeshForcing(17, 3600)
+    wave_forcing = WaveWatch3DataForcing(5, 3600)
+
+    nems = ModelingSystem(
+        start_time=datetime(2012, 10, 22, 6),
+        duration=timedelta(days=14.5),
+        interval=timedelta(hours=1),
+        atm=AtmosphericMeshEntry('/work/07531/zrb/stampede2/forcings/hsofs/SANDY_HWRF_HSOFS_Nov2018.nc'),
+        wav=WaveMeshEntry('/work/07531/zrb/stampede2/forcings/hsofs/ww3.HWRF.NOV2018.2012_sxy.nc'),
+        ocn=ADCIRCEntry(382),
+    )
+
+    nems.connect('ATM', 'OCN')
+    nems.connect('WAV', 'OCN')
+    nems.sequence = [
+        'ATM -> OCN',
+        'WAV -> OCN',
+        'ATM',
+        'WAV',
+        'OCN',
+    ]
+
+    write_adcirc_configurations(
+        nems,
+        runs,
+        INPUT_DIRECTORY,
+        OUTPUT_DIRECTORY,
+        name='nems_hsofs_test',
+        email_address='zachary.burnett@noaa.gov',
+        platform=HPC.STAMPEDE2,
+        spinup=timedelta(days=12.5),
+        forcings=[tidal_forcing, wind_forcing, wave_forcing],
+    )
