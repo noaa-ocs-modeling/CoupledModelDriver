2cbfd60b7814c554e1a2dd9f3bce9d07ab1462fe
zachary.burnett <zachary.burnett@noaa.gov>
2021-02-01 14:24:59 -0500

    fix last commit

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ logging.info(f'using version {version}')
 
 metadata = config.read_configuration('setup.cfg')['metadata']
 
-subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'adcircpy' '--upgrade'])
+subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'adcircpy', '--upgrade'])
 
 setup(
     name=metadata['name'],
