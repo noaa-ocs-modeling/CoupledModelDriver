4e89e2f106d15b48c6dc46fff8d54cc963367e9f
zachary.burnett <zachary.burnett@noaa.gov>
2021-02-17 15:56:00 -0500

    echo command for ease of use

--- coupledmodeldriver/job_script.py
+++ coupledmodeldriver/job_script.py
@@ -419,7 +419,11 @@ class RunScript(Script):
 
         if self.platform != Platform.LOCAL:
             # slurm queue output https://slurm.schedmd.com/squeue.html
-            lines.append('squeue -u $USER -o "%.8F %.21j %.4C %.4D %.31E %.7a %.9P %.20V %.20S %.20e"')
+            squeue_command = 'squeue -u $USER -o "%.8F %.21j %.4C %.4D %.31E %.7a %.9P %.20V %.20S %.20e"'
+            lines.extend([
+                f'echo {squeue_command}',
+                squeue_command,
+            ])
 
         return '\n'.join(lines)
 
