3204a3b0bc06679faa0a5aee1a1f5be601068772
zachary.burnett <zachary.burnett@noaa.gov>
2021-02-18 09:53:49 -0500

    absolute script path

--- coupledmodeldriver/job_script.py
+++ coupledmodeldriver/job_script.py
@@ -384,35 +384,37 @@ class RunScript(Script):
 
     def __str__(self) -> str:
         lines = [
+            'DIRECTORY="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"',
+            '',
             '# prepare single coldstart directory',
-            f'cd coldstart',
+            f'cd $DIRECTORY/coldstart',
             f'ln -sf ../{self.platform.value}_adcprep.job adcprep.job',
             f'ln -sf ../{self.platform.value}_nems_adcirc.job.coldstart nems_adcirc.job',
-            'cd ..',
+            'cd $DIRECTORY',
             '',
             '# prepare every hotstart directory',
             bash_for_loop(
-                'for hotstart in ./runs/*/',
+                'for hotstart in $DIRECTORY//runs/*/',
                 [
                     'cd "$hotstart"',
                     f'ln -sf ../../{self.platform.value}_adcprep.job adcprep.job',
                     f'ln -sf ../../{self.platform.value}_nems_adcirc.job.hotstart nems_adcirc.job',
-                    'cd ../..',
+                    'cd $DIRECTORY/',
                 ]
             ),
             '',
             '# run single coldstart configuration',
-            'cd coldstart',
+            'cd $DIRECTORY/coldstart',
             self.coldstart,
-            'cd ..',
+            'cd $DIRECTORY',
             '',
             '# run every hotstart configuration',
             bash_for_loop(
-                'for hotstart in ./runs/*/',
+                'for hotstart in $DIRECTORY/runs/*/',
                 [
                     'cd "$hotstart"',
                     self.hotstart,
-                    'cd ../..',
+                    'cd $DIRECTORY',
                 ]
             ),
         ]
