b21b53706d7ea89b29733951d943348556261552
zachary.burnett <zachary.burnett@noaa.gov>
2021-02-23 10:09:43 -0500

    update forcing file

--- examples/hera/hera_hsofs_sandy.py
+++ examples/hera/hera_hsofs_sandy.py
@@ -33,7 +33,7 @@ if __name__ == '__main__':
         start_time=datetime(2012, 10, 22, 6),
         end_time=datetime(2012, 10, 22, 6) + timedelta(days=14.5),
         interval=timedelta(hours=1),
-        atm=AtmosphericMeshEntry(FORCINGS_DIRECTORY / 'SANDY_HWRF_HSOFS_Nov2018.nc'),
+        atm=AtmosphericMeshEntry(FORCINGS_DIRECTORY / 'Wind_HWRF_SANDY_Nov2018_ExtendedSmoothT.nc'),
         wav=WaveMeshEntry(FORCINGS_DIRECTORY / 'ww3.HWRF.NOV2018.2012_sxy.nc'),
         ocn=ADCIRCEntry(382),
     )
