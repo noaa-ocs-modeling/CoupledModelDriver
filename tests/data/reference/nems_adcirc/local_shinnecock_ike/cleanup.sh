5cfabd51750a68e486b26336a478b5461afc2489
zachary.burnett <zachary.burnett@noaa.gov>
2021-04-29 08:15:34 -0400

    update reference files

--- tests/data/reference/nems_adcirc/local_shinnecock_ike/cleanup.sh
+++ tests/data/reference/nems_adcirc/local_shinnecock_ike/cleanup.sh
@@ -2,16 +2,12 @@ DIRECTORY="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P)"
 
 # clean spinup files
 pushd ${DIRECTORY}/spinup >/dev/null 2>&1
-rm -rf PE* ADC_*
-rm max* partmesh.txt metis_graph.txt
-rm fort.16 fort.6* fort.80
+rm -rf PE* ADC_* max* partmesh.txt metis_graph.txt fort.16 fort.6* fort.80
 popd >/dev/null 2>&1
 
 # clean run configurations
 for hotstart in ${DIRECTORY}/runs/*/; do
     pushd ${hotstart} >/dev/null 2>&1
-    rm -rf PE* ADC_*
-    rm max* partmesh.txt metis_graph.txt
-    rm fort.16 fort.6* fort.80
+    rm -rf PE* ADC_* max* partmesh.txt metis_graph.txt fort.16 fort.6* fort.80
     popd >/dev/null 2>&1
 done
