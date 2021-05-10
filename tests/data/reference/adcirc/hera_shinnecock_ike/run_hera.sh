91e0a576f71504fc3503a03fd9d83437e2451ba3
zachary.burnett <zachary.burnett@noaa.gov>
2021-05-10 13:20:52 -0400

    update reference files

--- tests/data/reference/adcirc/hera_shinnecock_ike/run_hera.sh
+++ tests/data/reference/adcirc/hera_shinnecock_ike/run_hera.sh
@@ -5,17 +5,17 @@ DIRECTORY="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P)"
 # run spinup
 pushd ${DIRECTORY}/spinup >/dev/null 2>&1
 setup_jobid=$(sbatch setup.job | awk '{print $NF}')
-spinup_jobid=$(sbatch adcirc.job --dependency=afterok:$setup_jobid | awk '{print $NF}')
+spinup_jobid=$(sbatch --dependency=afterok$setup_jobid adcirc.job | awk '{print $NF}')
 popd >/dev/null 2>&1
 
 # run configurations
 for hotstart in ${DIRECTORY}/runs/*/; do
     pushd ${hotstart} >/dev/null 2>&1
     setup_jobid=$(sbatch setup.job | awk '{print $NF}')
-    sbatch adcirc.job --dependency=afterok:$setup_jobid,afterok:$spinup_jobid
+    sbatch --dependency=afterok:$setup_jobid:$spinup_jobid adcirc.job
     popd >/dev/null 2>&1
 done
 
 # display job queue with dependencies
-squeue -u $USER -o "%.8i %3C %4D %97Z %15j" --sort i
-echo squeue -u $USER -o \"%.8i %3C %4D %97Z %15j\" --sort i
+squeue -u $USER -o "%.8i %3C %4D %15j %16E %Z" --sort i
+echo squeue -u $USER -o \"%.8i %3C %4D %15j %16E %Z\" --sort i
--- tests/data/reference/nems_adcirc/hera_shinnecock_ike_nospinup/run_hera.sh
+++ tests/data/reference/nems_adcirc/hera_shinnecock_ike_nospinup/run_hera.sh
@@ -6,10 +6,10 @@ DIRECTORY="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P)"
 for hotstart in ${DIRECTORY}/runs/*/; do
     pushd ${hotstart} >/dev/null 2>&1
     setup_jobid=$(sbatch setup.job | awk '{print $NF}')
-    sbatch adcirc.job --dependency=afterok:$setup_jobid
+    sbatch --dependency=afterok:$setup_jobid adcirc.job
     popd >/dev/null 2>&1
 done
 
 # display job queue with dependencies
-squeue -u $USER -o "%.8i %3C %4D %97Z %15j" --sort i
-echo squeue -u $USER -o \"%.8i %3C %4D %97Z %15j\" --sort i
+squeue -u $USER -o "%.8i %3C %4D %15j %16E %Z" --sort i
+echo squeue -u $USER -o \"%.8i %3C %4D %15j %16E %Z\" --sort i
