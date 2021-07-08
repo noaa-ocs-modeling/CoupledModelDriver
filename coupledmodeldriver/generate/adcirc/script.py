1da22a9ffd2c5f99a9c63f7e5809d411eb331f15
zachary.burnett <zachary.burnett@noaa.gov>
2021-07-08 08:55:03 -0400

    add `aswip` options from @WPringle

--- coupledmodeldriver/generate/adcirc/script.py
+++ coupledmodeldriver/generate/adcirc/script.py
@@ -131,7 +131,13 @@ class AdcircSetupJob(AdcircJob):
 
             setup_commands.extend(
                 [
-                    f'{self.aswip_path}',
+                    '## make sure ATCF format is correct for GAHM',
+                    '# ASWIP option descriptor',
+                    '#-n = nws option',
+                    '#-m = methods using isotachs, 1 = use the 34 isotach, 2 = use the 64 isotach, 3 = use the 50 isotach, 4 = use all available isotachs (use 4 for NWS=20)',
+                    '#-z = approaches solving for Rmax, 1 = only rotate wind vectors afterward, 2 = rotate wind vectors before and afterwards (use this for NWS=20)',
+                    '# - @wpringle',
+                    f'{self.aswip_path} -n 20 -m 4 -z 2',
                     'mv fort.22 fort.22.original',
                     'mv NWS_20_fort.22 fort.22',
                 ]
