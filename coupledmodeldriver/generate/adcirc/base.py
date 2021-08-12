192d4e155ab46d33fa4b183b100840bd605edaad
zachary.burnett <zachary.burnett@noaa.gov>
2021-08-12 17:25:13 -0400

    open mesh on-demand

--- coupledmodeldriver/generate/adcirc/base.py
+++ coupledmodeldriver/generate/adcirc/base.py
@@ -354,15 +354,14 @@ class ADCIRCJSON(ModelJSON, NEMSCapJSON, AttributeJSON):
     @property
     def base_mesh(self) -> AdcircMesh:
         if self.__base_mesh is None:
-            LOGGER.info(f'opening mesh "{self["fort_14_path"]}"')
-            self.__base_mesh = AdcircMesh.open(self['fort_14_path'], crs=4326)
+            self.__base_mesh = self['fort_14_path']
+        if not isinstance(self.__base_mesh, AdcircMesh):
+            LOGGER.info(f'opening mesh "{self.__base_mesh}"')
+            self.__base_mesh = AdcircMesh.open(self.__base_mesh, crs=4326)
         return self.__base_mesh
 
     @base_mesh.setter
     def base_mesh(self, base_mesh: Union[AdcircMesh, PathLike]):
-        if not isinstance(base_mesh, AdcircMesh):
-            self['fort_14_path'] = base_mesh
-            base_mesh = self.base_mesh
         self.__base_mesh = base_mesh
 
     @property
