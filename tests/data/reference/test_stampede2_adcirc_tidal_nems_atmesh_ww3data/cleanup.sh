38e785c600d8b21eee2f24d3f013c1fcb7d061e6
zachary.burnett <zachary.burnett@noaa.gov>
2021-08-19 12:40:23 -0400

    update tests

---
+++ tests/data/reference/test_stampede2_adcirc_tidal_nems_atmesh_ww3data/cleanup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+DIRECTORY="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P)"
+
+# clean spinup files
+pushd ${DIRECTORY}/spinup >/dev/null 2>&1
+rm -rf PE* ADC_* max* partmesh.txt metis_graph.txt fort.16 fort.6* fort.80
+popd >/dev/null 2>&1
+
+# clean run configurations
+for hotstart in ${DIRECTORY}/runs/*/; do
+    pushd ${hotstart} >/dev/null 2>&1
+    rm -rf PE* ADC_* max* partmesh.txt metis_graph.txt fort.16 fort.63 fort.64 fort.80
+    popd >/dev/null 2>&1
+done
--- tests/test_generation.py
+++ tests/test_generation.py
@@ -1,29 +1,25 @@
 from datetime import datetime, timedelta
 
-from adcircpy.forcing import BestTrackForcing
-from adcircpy.forcing.tides import Tides
-from adcircpy.forcing.tides.tides import TidalSource
+from adcircpy.forcing.tides.tides import TidalSource, Tides
 from adcircpy.forcing.waves.ww3 import WaveWatch3DataForcing
 from adcircpy.forcing.winds.atmesh import AtmosphericMeshForcing
+from adcircpy.forcing.winds.best_track import BestTrackForcing
 
 from coupledmodeldriver import Platform
-from coupledmodeldriver.generate import (
-    ADCIRCRunConfiguration,
-    generate_adcirc_configuration,
-    NEMSADCIRCRunConfiguration,
-)
-from tests import (
-    check_reference_directory,
-    INPUT_DIRECTORY,
-    OUTPUT_DIRECTORY,
-    REFERENCE_DIRECTORY,
-)
+from coupledmodeldriver.generate import (ADCIRCRunConfiguration,
+                                         NEMSADCIRCRunConfiguration,
+                                         generate_adcirc_configuration)
+from tests import (INPUT_DIRECTORY, OUTPUT_DIRECTORY,
+                   REFERENCE_DIRECTORY, check_reference_directory)
 
 NEMS_PATH = 'NEMS.x'
 ADCPREP_PATH = 'adcprep'
 
 
-def test_nems_adcirc_local_shinnecock_ike():
+def test_local_adcirc_tidal_nems_atmesh_ww3data():
+    output_directory = OUTPUT_DIRECTORY / 'test_local_adcirc_tidal_nems_atmesh_ww3data'
+    reference_directory = REFERENCE_DIRECTORY / 'test_local_adcirc_tidal_nems_atmesh_ww3data'
+
     platform = Platform.LOCAL
     mesh = 'shinnecock'
     storm = 'ike'
@@ -38,13 +34,6 @@ def test_nems_adcirc_local_shinnecock_ike():
     input_directory = INPUT_DIRECTORY / mesh
     mesh_directory = input_directory / 'mesh'
     forcings_directory = input_directory / storm / 'forcings'
-
-    output_directory = (
-        OUTPUT_DIRECTORY / 'nems_adcirc' / f'{platform.name.lower()}_{mesh}_{storm}'
-    )
-    reference_directory = (
-        REFERENCE_DIRECTORY / 'nems_adcirc' / f'{platform.name.lower()}_{mesh}_{storm}'
-    )
 
     nems_connections = ['ATM -> OCN', 'WAV -> OCN']
     nems_mediations = None
@@ -110,7 +99,10 @@ def test_nems_adcirc_local_shinnecock_ike():
     )
 
 
-def test_nems_adcirc_hera_shinnecock_ike():
+def test_hera_adcirc_tidal_nems_atmesh_ww3data():
+    output_directory = OUTPUT_DIRECTORY / 'test_hera_adcirc_tidal_nems_atmesh_ww3data'
+    reference_directory = REFERENCE_DIRECTORY / 'test_hera_adcirc_tidal_nems_atmesh_ww3data'
+
     platform = Platform.HERA
     mesh = 'shinnecock'
     storm = 'ike'
@@ -125,13 +117,6 @@ def test_nems_adcirc_hera_shinnecock_ike():
     input_directory = INPUT_DIRECTORY / mesh
     mesh_directory = input_directory / 'mesh'
     forcings_directory = input_directory / storm / 'forcings'
-
-    output_directory = (
-        OUTPUT_DIRECTORY / 'nems_adcirc' / f'{platform.name.lower()}_{mesh}_{storm}'
-    )
-    reference_directory = (
-        REFERENCE_DIRECTORY / 'nems_adcirc' / f'{platform.name.lower()}_{mesh}_{storm}'
-    )
 
     nems_connections = ['ATM -> OCN', 'WAV -> OCN']
     nems_mediations = None
@@ -199,7 +184,12 @@ def test_nems_adcirc_hera_shinnecock_ike():
     )
 
 
-def test_nems_adcirc_stampede2_shinnecock_ike():
+def test_stampede2_adcirc_tidal_nems_atmesh_ww3data():
+    output_directory = OUTPUT_DIRECTORY / 'test_stampede2_adcirc_tidal_nems_atmesh_ww3data'
+    reference_directory = (
+        REFERENCE_DIRECTORY / 'test_stampede2_adcirc_tidal_nems_atmesh_ww3data'
+    )
+
     platform = Platform.STAMPEDE2
     mesh = 'shinnecock'
     storm = 'ike'
@@ -214,13 +204,6 @@ def test_nems_adcirc_stampede2_shinnecock_ike():
     input_directory = INPUT_DIRECTORY / mesh
     mesh_directory = input_directory / 'mesh'
     forcings_directory = input_directory / storm / 'forcings'
-
-    output_directory = (
-        OUTPUT_DIRECTORY / 'nems_adcirc' / f'{platform.name.lower()}_{mesh}_{storm}'
-    )
-    reference_directory = (
-        REFERENCE_DIRECTORY / 'nems_adcirc' / f'{platform.name.lower()}_{mesh}_{storm}'
-    )
 
     nems_connections = ['ATM -> OCN', 'WAV -> OCN']
     nems_mediations = None
@@ -288,10 +271,12 @@ def test_nems_adcirc_stampede2_shinnecock_ike():
     )
 
 
-def test_adcirc_local_shinnecock_ike():
+def test_local_adcirc_tidal():
+    output_directory = OUTPUT_DIRECTORY / 'test_local_adcirc_tidal'
+    reference_directory = REFERENCE_DIRECTORY / 'test_local_adcirc_tidal'
+
     platform = Platform.LOCAL
     mesh = 'shinnecock'
-    storm = 'ike'
     adcirc_processors = 11
     modeled_start_time = datetime(2008, 8, 23)
     modeled_duration = timedelta(days=14.5)
@@ -301,11 +286,6 @@ def test_adcirc_local_shinnecock_ike():
 
     input_directory = INPUT_DIRECTORY / mesh
     mesh_directory = input_directory / 'mesh'
-
-    output_directory = OUTPUT_DIRECTORY / 'adcirc' / f'{platform.name.lower()}_{mesh}_{storm}'
-    reference_directory = (
-        REFERENCE_DIRECTORY / 'adcirc' / f'{platform.name.lower()}_{mesh}_{storm}'
-    )
 
     slurm_email_address = 'example@email.gov'
 
@@ -349,10 +329,12 @@ def test_adcirc_local_shinnecock_ike():
     )
 
 
-def test_adcirc_hera_shinnecock_ike():
+def test_hera_adcirc_tidal():
+    output_directory = OUTPUT_DIRECTORY / 'test_hera_adcirc_tidal'
+    reference_directory = REFERENCE_DIRECTORY / 'test_hera_adcirc_tidal'
+
     platform = Platform.HERA
     mesh = 'shinnecock'
-    storm = 'ike'
     adcirc_processors = 15 * platform.value['processors_per_node']
     modeled_start_time = datetime(2008, 8, 23)
     modeled_duration = timedelta(days=14.5)
@@ -362,11 +344,6 @@ def test_adcirc_hera_shinnecock_ike():
 
     input_directory = INPUT_DIRECTORY / mesh
     mesh_directory = input_directory / 'mesh'
-
-    output_directory = OUTPUT_DIRECTORY / 'adcirc' / f'{platform.name.lower()}_{mesh}_{storm}'
-    reference_directory = (
-        REFERENCE_DIRECTORY / 'adcirc' / f'{platform.name.lower()}_{mesh}_{storm}'
-    )
 
     slurm_email_address = 'example@email.gov'
 
@@ -410,10 +387,12 @@ def test_adcirc_hera_shinnecock_ike():
     )
 
 
-def test_adcirc_stampede2_shinnecock_ike():
+def test_stampede2_adcirc_tidal():
+    output_directory = OUTPUT_DIRECTORY / 'test_stampede2_adcirc_tidal'
+    reference_directory = REFERENCE_DIRECTORY / 'test_stampede2_adcirc_tidal'
+
     platform = Platform.STAMPEDE2
     mesh = 'shinnecock'
-    storm = 'ike'
     adcirc_processors = 15 * platform.value['processors_per_node']
     modeled_start_time = datetime(2008, 8, 23)
     modeled_duration = timedelta(days=14.5)
@@ -423,11 +402,6 @@ def test_adcirc_stampede2_shinnecock_ike():
 
     input_directory = INPUT_DIRECTORY / mesh
     mesh_directory = input_directory / 'mesh'
-
-    output_directory = OUTPUT_DIRECTORY / 'adcirc' / f'{platform.name.lower()}_{mesh}_{storm}'
-    reference_directory = (
-        REFERENCE_DIRECTORY / 'adcirc' / f'{platform.name.lower()}_{mesh}_{storm}'
-    )
 
     slurm_email_address = 'example@email.gov'
 
@@ -471,7 +445,10 @@ def test_adcirc_stampede2_shinnecock_ike():
     )
 
 
-def test_nems_adcirc_hera_shinnecock_ike_nospinup():
+def test_hera_adcirc_nems_atmesh_ww3data():
+    output_directory = OUTPUT_DIRECTORY / 'test_hera_adcirc_nems_atmesh_ww3data'
+    reference_directory = REFERENCE_DIRECTORY / 'test_hera_adcirc_nems_atmesh_ww3data'
+
     platform = Platform.HERA
     mesh = 'shinnecock'
     storm = 'ike'
@@ -486,15 +463,6 @@ def test_nems_adcirc_hera_shinnecock_ike_nospinup():
     input_directory = INPUT_DIRECTORY / mesh
     mesh_directory = input_directory / 'mesh'
     forcings_directory = input_directory / storm / 'forcings'
-
-    output_directory = (
-        OUTPUT_DIRECTORY / 'nems_adcirc' / f'{platform.name.lower()}_{mesh}_{storm}_nospinup'
-    )
-    reference_directory = (
-        REFERENCE_DIRECTORY
-        / 'nems_adcirc'
-        / f'{platform.name.lower()}_{mesh}_{storm}_nospinup'
-    )
 
     nems_connections = ['ATM -> OCN', 'WAV -> OCN']
     nems_mediations = None
@@ -562,10 +530,12 @@ def test_nems_adcirc_hera_shinnecock_ike_nospinup():
     )
 
 
-def test_adcirc_hera_shinnecock_ike_nospinup():
+def test_hera_adcirc():
+    output_directory = OUTPUT_DIRECTORY / 'test_hera_adcirc'
+    reference_directory = REFERENCE_DIRECTORY / 'test_hera_adcirc'
+
     platform = Platform.HERA
     mesh = 'shinnecock'
-    storm = 'ike'
     adcirc_processors = 15 * platform.value['processors_per_node']
     modeled_start_time = datetime(2008, 8, 23)
     modeled_duration = timedelta(days=14.5)
@@ -576,18 +546,7 @@ def test_adcirc_hera_shinnecock_ike_nospinup():
     input_directory = INPUT_DIRECTORY / mesh
     mesh_directory = input_directory / 'mesh'
 
-    output_directory = (
-        OUTPUT_DIRECTORY / 'adcirc' / f'{platform.name.lower()}_{mesh}_{storm}_nospinup'
-    )
-    reference_directory = (
-        REFERENCE_DIRECTORY / 'adcirc' / f'{platform.name.lower()}_{mesh}_{storm}_nospinup'
-    )
-
     slurm_email_address = 'example@email.gov'
-
-    tidal_forcing = Tides(tidal_source=TidalSource.HAMTIDE)
-    tidal_forcing.use_all()
-    forcings = [tidal_forcing]
 
     configuration = ADCIRCRunConfiguration(
         mesh_directory=mesh_directory,
@@ -597,7 +556,7 @@ def test_adcirc_hera_shinnecock_ike_nospinup():
         tidal_spinup_duration=tidal_spinup_duration,
         platform=platform,
         perturbations=None,
-        forcings=forcings,
+        forcings=None,
         adcirc_processors=adcirc_processors,
         slurm_partition=None,
         slurm_job_duration=job_duration,
@@ -625,7 +584,14 @@ def test_adcirc_hera_shinnecock_ike_nospinup():
     )
 
 
-def test_nems_adcirc_hera_shinnecock_ike_perturbed():
+def test_hera_adcirc_tidal_nems_atmesh_ww3data_perturbed():
+    output_directory = (
+        OUTPUT_DIRECTORY / 'test_hera_adcirc_tidal_nems_atmesh_ww3data_perturbed'
+    )
+    reference_directory = (
+        REFERENCE_DIRECTORY / 'test_hera_adcirc_tidal_nems_atmesh_ww3data_perturbed'
+    )
+
     platform = Platform.HERA
     mesh = 'shinnecock'
     storm = 'ike'
@@ -640,15 +606,6 @@ def test_nems_adcirc_hera_shinnecock_ike_perturbed():
     input_directory = INPUT_DIRECTORY / mesh
     mesh_directory = input_directory / 'mesh'
     forcings_directory = input_directory / storm / 'forcings'
-
-    output_directory = (
-        OUTPUT_DIRECTORY / 'nems_adcirc' / f'{platform.name.lower()}_{mesh}_{storm}_perturbed'
-    )
-    reference_directory = (
-        REFERENCE_DIRECTORY
-        / 'nems_adcirc'
-        / f'{platform.name.lower()}_{mesh}_{storm}_perturbed'
-    )
 
     nems_connections = ['ATM -> OCN', 'WAV -> OCN']
     nems_mediations = None
@@ -726,7 +683,10 @@ def test_nems_adcirc_hera_shinnecock_ike_perturbed():
     )
 
 
-def test_nems_adcirc_hera_shinnecock_ike_aswip():
+def test_hera_adcirc_tidal_besttrack_nems_ww3data():
+    output_directory = OUTPUT_DIRECTORY / 'test_hera_adcirc_tidal_besttrack_nems_ww3data'
+    reference_directory = REFERENCE_DIRECTORY / 'test_hera_adcirc_tidal_besttrack_nems_ww3data'
+
     platform = Platform.HERA
     mesh = 'shinnecock'
     storm = 'ike'
@@ -742,12 +702,87 @@ def test_nems_adcirc_hera_shinnecock_ike_aswip():
     mesh_directory = input_directory / 'mesh'
     forcings_directory = input_directory / storm / 'forcings'
 
-    output_directory = (
-        OUTPUT_DIRECTORY / 'nems_adcirc' / f'{platform.name.lower()}_{mesh}_{storm}_aswip'
+    nems_connections = ['WAV -> OCN']
+    nems_mediations = None
+    nems_sequence = [
+        'WAV -> OCN',
+        'WAV',
+        'OCN',
+    ]
+
+    slurm_email_address = 'example@email.gov'
+
+    tidal_forcing = Tides(tidal_source=TidalSource.HAMTIDE)
+    tidal_forcing.use_all()
+    wind_forcing = BestTrackForcing(storm='ike2008', nws=8, interval_seconds=3600)
+    wave_forcing = WaveWatch3DataForcing(
+        filename=forcings_directory / 'ww3.Constant.20151214_sxy_ike_date.nc',
+        nrs=5,
+        interval_seconds=3600,
     )
+    forcings = [tidal_forcing, wind_forcing, wave_forcing]
+
+    configuration = NEMSADCIRCRunConfiguration(
+        mesh_directory=mesh_directory,
+        modeled_start_time=modeled_start_time,
+        modeled_end_time=modeled_start_time + modeled_duration,
+        modeled_timestep=modeled_timestep,
+        nems_interval=nems_interval,
+        nems_connections=nems_connections,
+        nems_mediations=nems_mediations,
+        nems_sequence=nems_sequence,
+        tidal_spinup_duration=tidal_spinup_duration,
+        platform=platform,
+        perturbations=None,
+        forcings=forcings,
+        adcirc_processors=adcirc_processors,
+        slurm_partition=None,
+        slurm_job_duration=job_duration,
+        slurm_email_address=slurm_email_address,
+        nems_executable=INPUT_DIRECTORY / 'bin' / 'NEMS.x',
+        adcprep_executable=INPUT_DIRECTORY / 'bin' / 'adcprep',
+        aswip_executable=None,
+        source_filename=INPUT_DIRECTORY / 'modulefiles' / 'envmodules_intel.hera',
+    )
+
+    configuration.relative_to(output_directory)
+
+    configuration.write_directory(output_directory, overwrite=True)
+    generate_adcirc_configuration(output_directory, relative_paths=True, overwrite=True)
+
+    check_reference_directory(
+        test_directory=output_directory,
+        reference_directory=reference_directory,
+        skip_lines={
+            'fort.15': [0],
+            'config.rc': [0],
+            'model_configure': [0],
+            'atm_namelist.rc': [0],
+            'nems.configure': [0],
+        },
+    )
+
+
+def test_hera_adcirc_tidal_besttrack_nems_ww3data_aswip():
+    output_directory = OUTPUT_DIRECTORY / 'test_hera_adcirc_tidal_besttrack_nems_ww3data_aswip'
     reference_directory = (
-        REFERENCE_DIRECTORY / 'nems_adcirc' / f'{platform.name.lower()}_{mesh}_{storm}_aswip'
+        REFERENCE_DIRECTORY / 'test_hera_adcirc_tidal_besttrack_nems_ww3data_aswip'
     )
+
+    platform = Platform.HERA
+    mesh = 'shinnecock'
+    storm = 'ike'
+    adcirc_processors = 15 * platform.value['processors_per_node']
+    modeled_start_time = datetime(2008, 9, 1, 6)
+    modeled_duration = timedelta(days=14)
+    modeled_timestep = timedelta(seconds=2)
+    tidal_spinup_duration = timedelta(days=12.5)
+    nems_interval = timedelta(hours=1)
+    job_duration = timedelta(hours=6)
+
+    input_directory = INPUT_DIRECTORY / mesh
+    mesh_directory = input_directory / 'mesh'
+    forcings_directory = input_directory / storm / 'forcings'
 
     nems_connections = ['WAV -> OCN']
     nems_mediations = None
